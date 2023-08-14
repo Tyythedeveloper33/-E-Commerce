5e8ae7d67a6993033dd4f262c6556ba45bdae0e0
unknown <kingwebbe@gmail.com>
2023-08-14 18:24:49 -0400

    fixed routes up to product post

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req,res) => {
   // be sure to include its associated Products
   try {
     const CategoryData = await Category.findAll({
-     // include: [{ model: Product }],
+      include: [{ model: Product }],
     });
     res.status(200).json(CategoryData);
   } catch (err) {
@@ -52,17 +52,49 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   console.log("Incoming Data: ", req.body)
   console.log("Incoming Request Parameter: ", req.params)
-  
+  try {
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!updatedCategory[0]) {
+     return res.status(404).json({ message: 'No user with this id!' });
+    } 
+   
+    return res.status(200).json(updatedCategory);
+   
+  } catch (err) {
+    return res.status(500).json(err);
+  } 
   // update a category by its `id` value
-  res.json({ msg: "Created New Record"})
 });
 
-router.delete('/:id', (req, res) => {
-  console.log("Incoming Request Parameter: ", req.params)
+
+
+
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No library card found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 module.exports = router;
