fdaedf0b7ebb0ae6bdd75f75d51a7e5ba226f5d4
unknown <kingwebbe@gmail.com>
2023-08-15 20:29:47 -0400

    fixed all routes, need to make readme to turn in project

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -49,11 +49,38 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => 
+    res.status(200).json(tag))
+    .catch((err)=>
+    res.status(404).json(err));
+           
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
-});
+
 
 module.exports = router;
